dc262863ee1ed6631596ad0d4a0e52e936454cb2
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-01 01:29:43 -0700

    Created Polkadot class and defined get_data function

--- polkadot_sklearn/data.py
+++ polkadot_sklearn/data.py
@@ -2,13 +2,19 @@ import pandas as pd
 import os
 
 class Polkadot:
-    def get_data(self):
-        csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
-        file_names = [f for f in os.listdir(csv_path) if f.endswith('.csv')]
-        print(file_names)
+    def get_data(self, on_chain=False):
 
-# coingecko = pd.read_csv('Polkadot_historical_data.csv')
-# polkadot = pd.read_csv('../data/Daily Active Account & Newly Created Account.csv')
-# print(polkadot.head())
-# print(coingecko.head())
+        data = {}
+        if on_chain == True:
+            csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+            file_names = [f for f in os.listdir(csv_path) if f.endswith('.csv')]
+            for i, j in enumerate(file_names):
+                file_names[i] = j.replace('.csv', '')
+            for (x, y) in zip(file_names, os.listdir(csv_path)[1:]):
+                data[x] = pd.read_csv(os.path.join(csv_path, y))
+
+        data['Coingecko'] = pd.read_csv('Polkadot_historical_data.csv')
+        print(data)
+
+
 print(Polkadot().get_data())
