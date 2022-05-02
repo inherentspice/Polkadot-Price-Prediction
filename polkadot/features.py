94affcf5aab4f1caba30847b7e3968522109e7a8
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-02 03:16:42 -0700

    wrote function to convert Bitcoin_fear_and_greed.csv into dataframe

--- polkadot/features.py
+++ polkadot/features.py
@@ -7,3 +7,11 @@ class Dataframe:
         df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
         df['date'] = pd.to_datetime(df['date'])
         return df
+
+    def get_fear_and_greed(self):
+        df = Polkadot().get_data().get('Fear_and_greed')
+        df.drop(columns='Unnamed: 0', inplace=True)
+        df['Date'] = pd.to_datetime(df['Date'])
+        df['Value_classification'] = df['Value_classification'].astype("str")
+        df.sort_values(by='Date', inplace=True)
+        return df
