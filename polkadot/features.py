73bdf1f26db1c20a259331ae22a53327ba72b0bc
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-01 23:49:07 -0700

    convert date column to datetime type

--- polkadot/features.py
+++ polkadot/features.py
@@ -1,7 +1,9 @@
 from data import Polkadot
+import pandas as pd
 
 class Dataframe:
     def get_coingecko(self):
         df = Polkadot().get_data().get('Coingecko')
         df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
+        df['date'] = pd.to_datetime(df['date'])
         return df
