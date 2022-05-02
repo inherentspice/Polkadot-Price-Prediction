09eff4ef0e5a644747fddac950baa325ee29df6c
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-02 03:47:18 -0700

    wrote function to merge dataframes into one dataframe of features

--- polkadot/features.py
+++ polkadot/features.py
@@ -2,16 +2,25 @@ from data import Polkadot
 import pandas as pd
 
 class Dataframe:
+    def __init__(self):
+        self.data = Polkadot().get_data()
+
     def get_coingecko(self):
-        df = Polkadot().get_data().get('Coingecko')
+        df = self.data.get('Coingecko')
         df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
         df['date'] = pd.to_datetime(df['date'])
         return df
 
     def get_fear_and_greed(self):
-        df = Polkadot().get_data().get('Fear_and_greed')
+        df = self.data.get('Fear_and_greed')
         df.drop(columns='Unnamed: 0', inplace=True)
         df['Date'] = pd.to_datetime(df['Date'])
-        df['Value_classification'] = df['Value_classification'].astype("str")
         df.sort_values(by='Date', inplace=True)
         return df
+
+    def get_dataframe(self):
+        gecko_df = self.get_coingecko()
+        fear_df = self.get_fear_and_greed()
+        df_merged = gecko_df.merge(fear_df, left_on='date', right_on='Date')
+        df_merged.drop(columns='Date', inplace=True)
+        return df_merged
