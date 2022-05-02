972ca072bf1c667984bdb990eb312f6a684fe78a
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-02 03:08:22 -0700

    added bitcoin fear and greed key to data

--- polkadot/data.py
+++ polkadot/data.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import os
+from coingecko_scraper import Scraper
+import csv
 
 class Polkadot:
     def get_data(self, on_chain=False):
         """ This function returns a Python dict.
         If 'on_chain' is set to True, the keys should be 'Treasury Available Income',
         'Daily Unbonding Schedule (DOT), etc.
-        Else it will have one key: 'Coingecko'
+        Else it will have two keys: 'Coingecko', 'Fear_and_greed'
         Its values should be pandas.DataFrames loaded from csv files """
         data = {}
 
@@ -19,7 +21,5 @@ class Polkadot:
                 data[x] = pd.read_csv(os.path.join(csv_path, y))
 
         data['Coingecko'] = pd.read_csv('Polkadot_historical_data.csv')
+        data['Fear_and_greed'] = pd.read_csv('Bitcoin_fear_and_greed.csv')
         return data
-
-    # def update_coingecko(self):
-    #     data = self.get_data(on_chain=False)
