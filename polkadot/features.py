3b19d2532afd4e6d1a1dfa30baf1c2e7bab834d7
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-03 03:19:15 -0700

    wrote function to create target and additional features

--- polkadot/features.py
+++ polkadot/features.py
@@ -1,5 +1,6 @@
 from data import Polkadot
 import pandas as pd
+import numpy as np
 
 class Dataframe:
     def __init__(self):
@@ -24,3 +25,28 @@ class Dataframe:
         df_merged = gecko_df.merge(fear_df, left_on='date', right_on='Date')
         df_merged.drop(columns='Date', inplace=True)
         return df_merged
+
+    def get_features_targets(self, classification=True):
+        features = self.get_dataframe()
+
+        # create target
+        features['predict'] = features['current_price'].shift(-2)
+        if classification == True:
+            features['predict'] = pd.Series(np.where(features['predict'].values < features['predict'].shift(-1), 0, 1),
+                                            features.index)
+
+        # create features
+        features['ema5'] = features['current_price'].ewm(span=5, adjust=False).mean()
+        features['ema20'] = features['current_price'].ewm(span=20, adjust=False).mean()
+        features['ema50'] = features['current_price'].ewm(span=50, adjust=False).mean()
+
+        features['fear_ema50'] = features['Value'].ewm(span=50, adjust=False).mean()
+        features['fear_change_2_weeks'] = features['Value'] - features['Value'].shift(-14)
+        features['reddit_post_ema5'] = features['reddit_post_48h'].ewm(span=5, adjust=False).mean()
+        features['reddit_change_2_weeks'] = features['reddit_post_48h'] - features['reddit_post_48h'].shift(-14)
+        features['sats_ema5'] = features['current_price_sats'].ewm(span=5, adjust=False).mean()
+        features['sats_ema20'] = features['current_price_sats'].ewm(span=20, adjust=False).mean()
+        features['sats_ema50'] = features['current_price_sats'].ewm(span=50, adjust=False).mean()
+        features['sats_change_3_days'] = features['current_price_sats'] - features['current_price_sats'].shift(-3)
+        # features.dropna(inplace=True)
+        return features
