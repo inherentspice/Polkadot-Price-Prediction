81a8fe738957389de3621eec001f314c50376fe3
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-04 02:32:56 -0700

    created function that takes x and y, and shows scores from different classification models

---
+++ polkadot/utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
+class Utils:
+    def test_classifiers(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        k_regressor = KNeighborsClassifier().fit(X_train, y_train)
+        s_model = SVC().fit(X_train, y_train)
+        classifier = DecisionTreeClassifier().fit(X_train, y_train)
+        random_classifier = RandomForestClassifier().fit(X_train, y_train)
+        ada = AdaBoostClassifier().fit(X_train, y_train)
+        gradient = GradientBoostingClassifier().fit(X_train, y_train)
+        scores = pd.DataFrame(columns=['scores'], index=['KNeighbors', 'Support Vector', 'Decision Tree',
+                                                'Random Forest', 'Ada Boost', 'Gradient Boost'],
+                      data=[k_regressor.score(X_test, y_test), s_model.score(X_test, y_test),
+                           classifier.score(X_test, y_test), random_classifier.score(X_test, y_test),
+                           ada.score(X_test, y_test), gradient.score(X_test, y_test)])
+        return scores
