d3e90ca0612625ea33a95c04b2f8f82db1541ea0
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-10 02:28:48 -0700

    added a validation set to the test_classifiers function

--- polkadot/utils.py
+++ polkadot/utils.py
@@ -12,16 +12,18 @@ class Utils:
         the names of classifiers, and their baseline score
         of predicting y given x.
         """
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        k_regressor = KNeighborsClassifier(random_state=42).fit(X_train, y_train)
-        s_model = SVC(random_state=42).fit(X_train, y_train)
-        classifier = DecisionTreeClassifier(random_state=42).fit(X_train, y_train)
-        random_classifier = RandomForestClassifier(random_state=42).fit(X_train, y_train)
-        ada = AdaBoostClassifier(random_state=42).fit(X_train, y_train)
-        gradient = GradientBoostingClassifier(random_state=42).fit(X_train, y_train)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                          test_size=0.25, random_state= 8)
+        k_regressor = KNeighborsClassifier().fit(X_train, y_train)
+        s_model = SVC().fit(X_train, y_train)
+        classifier = DecisionTreeClassifier().fit(X_train, y_train)
+        random_classifier = RandomForestClassifier().fit(X_train, y_train)
+        ada = AdaBoostClassifier().fit(X_train, y_train)
+        gradient = GradientBoostingClassifier().fit(X_train, y_train)
         scores = pd.DataFrame(columns=['scores'], index=['KNeighbors', 'Support Vector', 'Decision Tree',
                                                 'Random Forest', 'Ada Boost', 'Gradient Boost'],
-                      data=[k_regressor.score(X_test, y_test), s_model.score(X_test, y_test),
-                           classifier.score(X_test, y_test), random_classifier.score(X_test, y_test),
-                           ada.score(X_test, y_test), gradient.score(X_test, y_test)])
+                      data=[k_regressor.score(X_val, y_val), s_model.score(X_val, y_val),
+                           classifier.score(X_val, y_val), random_classifier.score(X_val, y_val),
+                           ada.score(X_val, y_val), gradient.score(X_val, y_val)])
         return scores
