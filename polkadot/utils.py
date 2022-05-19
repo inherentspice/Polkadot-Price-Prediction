42ed3ecf555de76864db4d8a2dbdcf617b08d967
Stephen Smith <stephenlehanesmith@gmail.com>
2022-05-19 04:57:01 -0700

    added explanatory comments to test_classifiers function

--- polkadot/utils.py
+++ polkadot/utils.py
@@ -14,14 +14,16 @@ class Utils:
         the names of classifiers, and their baseline score
         of predicting y given x.
         """
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        scaler = RobustScaler().fit(X_train)
-        X_train_scaled = scaler.transform(X_train)
 
+        # split data, impute missing values, scale data for models
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         imp_simple = SimpleImputer()
         imp_simple.fit(X_train)
-        X_train_scaled = imp_simple.transform(X_train)
+        X_train_impute = imp_simple.transform(X_train)
+        scaler = RobustScaler().fit(X_train_impute)
+        X_train_scaled = scaler.transform(X_train_impute)
 
+        # instantiate different classifiers
         k_regressor = KNeighborsClassifier()
         s_model = SVC()
         classifier = DecisionTreeClassifier()
@@ -29,6 +31,7 @@ class Utils:
         ada = AdaBoostClassifier()
         gradient = GradientBoostingClassifier()
 
+        # calculate the cross validation score for each model
         score_k = cross_val_score(k_regressor, X_train_scaled, y_train, cv=5)
         score_s = cross_val_score(s_model, X_train_scaled, y_train, cv=5)
         score_c = cross_val_score(classifier, X_train_scaled, y_train, cv=5)
@@ -36,6 +39,7 @@ class Utils:
         score_a = cross_val_score(ada, X_train_scaled, y_train, cv=5)
         score_g = cross_val_score(gradient, X_train_scaled, y_train, cv=5)
 
+        # create a DataFrame that contains the name and score of each model
         scores = pd.DataFrame(columns=['scores'], index=['KNeighbors', 'Support Vector', 'Decision Tree',
                                                 'Random Forest', 'Ada Boost', 'Gradient Boost'],
                       data=[score_k.mean(), score_s.mean(), score_c.mean(), score_rc.mean(),
